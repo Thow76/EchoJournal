aec11bb36ac2c086193df839823693ca6b5e95b9
Andrew Thow <thow1976@hotmail.co.uk>
2025-01-03 11:58:29 +0000

    1.3 journal entry data class set up

---
+++ app/src/main/java/com/example/echojournal/data/JournalEntry.kt
@@ -0,0 +1,13 @@
+package com.example.echojournal.data
+
+data class JournalEntry(
+    val id: Int,
+    val title: String,
+    val date: String, // Example: "Today", "Yesterday", or "Saturday, Dec 28"
+    val mood: Mood,
+    val description: String
+)
+
+
+
+enum class Mood { STRESSED, SAD, NEUTRAL, PEACEFUL, EXCITED }
